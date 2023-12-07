961e77794fea6c7b62a8ddadd2f82e931e33cf43
1f10220167 <s1f102201672@iniad.org>
2023-12-07 11:22:09 +0900

    added a docker-compose file for gce-2

--- src/config/urls.py
+++ src/config/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from testapp.views import root, page2  # testappを正しくインポート
 
 urlpatterns = [
     path('admin/', admin.site.urls),
