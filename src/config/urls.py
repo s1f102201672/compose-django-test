9d492077649fa85098d1b857b1460a606aa0e14a
1f10220167 <s1f102201672@iniad.org>
2023-12-07 11:01:50 +0900

    added a docker-compose file for gce

--- src/config/urls.py
+++ src/config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('top/', testapp.views.root),
+    path('page2/', page2),
 ]
--- src/testapp/views.py
+++ src/testapp/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def root(request):
     return HttpResponse('Hello Django and Docker')
+
+def page2(request):
+    return render(request, 'page2.html')
